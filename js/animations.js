f0db40c38910e27fc1ac86da0ef32a02096a261f
venzard <vasilij-ch@ya.ru>
2022-11-01 21:53:19 +0300

    feat: budget select 'other'

--- js/animations.js
+++ js/animations.js
@@ -19,3 +19,23 @@ function initIncreaseNumberAnimation() {
 }
 
 initIncreaseNumberAnimation();
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input');
+
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text"; 
+      
+    formContainer.appendChild(input);
+    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+  }
+ 
+  const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) {
+  document.querySelector('#form form').removeChild(otherInput);
+  }
+});
